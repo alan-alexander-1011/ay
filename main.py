2cadbd4a9486323170b5d89a26f1b03382465eb0
alan-alexander-1011 <chungocthaibao@gmail.com>
2024-08-15 01:45:04 +0700

    fix logic

--- main.py
+++ main.py
@@ -9,12 +9,7 @@ def help():
   print('update / upgrade : used to update all of your packages and your core files.')
   print('remove / uninstall : used to remove packages.')
   print('help : show this help tab.')
-
-if command not in ['install', 'sync', 'update', 'upgrade', 'remove', 'uninstall', 'help']:
-  print("Invalid commands. The commands are:")
-  help()
-  exit(-1)
-
+  
 if command in ['install', 'sync']:
   ret = install_pkg(args)
   print(ret)
@@ -22,3 +17,9 @@ elif command in ['update', 'upgrade']:
   subprocess.run(['sudo', 'pacman', '-Syu'] + args)
 elif command in ['remove', 'uninstall']:
   subprocess.run(['sudo', 'pacman', '-R'] + args)
+elif command == 'help':
+  help()
+else:
+  print("Invalid commands. The commands are:")
+  help()
+  exit(-1)
